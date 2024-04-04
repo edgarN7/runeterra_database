aaabe839b64634f33cb726ee0e59b0224b6568bf
edgarn7 <edgar17.neto@gmail.com>
2024-04-04 19:21:19 -0300

    updating the characters on bandle city repository.

--- lib/bandle_city/bandle_city_characters_repository.dart
+++ lib/bandle_city/bandle_city_characters_repository.dart
@@ -5,8 +5,9 @@ class BandleCityCharactersRepository {
     Characters(1, "Corki", "corki.txt"),
     Characters(2, "Lulu", "lulu.txt"),
     Characters(3, "Rumble", "rumble.txt"),
-
+    Characters(4, "Teemo", "teemo.txt"),
+    Characters(5, "Tristana", "tristana.txt"),
+    Characters(6, "Veigar", "veigar.txt"),
+    Characters(7, "Yuumi", "yuumi.txt"),
   ];
-
-
 }
