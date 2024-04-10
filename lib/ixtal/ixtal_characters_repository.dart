83d8633642f3ccbad51d9014fc8ce87fb90fc6a0
edgarn7 <edgar17.neto@gmail.com>
2024-04-10 15:30:37 -0300

    adding Ixtal.

---
+++ lib/ixtal/ixtal_characters_repository.dart
@@ -0,0 +1,14 @@
+import '../../characters.dart';
+
+class IxtalCharactersRepository {
+  static List<Characters> ixtalCharactersTable = [
+    Characters(1, "Malphite", "malphite.txt"),
+    Characters(2, "Milio", "milio.txt"),
+    Characters(3, "Neeko", "neeko.txt"),
+    Characters(4, "Nidalee", "nidalee.txt"),
+    Characters(5, "Qiyana", "qiyana.txt"),
+    Characters(6, "Rengar", "rengar.txt"),
+    Characters(7, "Skarner", "skarner.txt"),
+    Characters(8, "Zyra", "zyra.txt"),
+  ];
+}
---
+++ lib/ixtal/ixtal_places_repository.dart
@@ -0,0 +1,9 @@
+import '../../places.dart';
+
+class IxtalPlacesRepository {
+  static List<Places> ixtalPlacesTable = [
+    Places(1, "Ixaocan", "ixaocan.txt"),
+    Places(2, "Selva do Sul", "selva_do_sul.txt"),
+    Places(3, "Rio Serpentina", "rio_serpentina.txt"),
+  ];
+}
