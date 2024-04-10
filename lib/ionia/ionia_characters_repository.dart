547c4d8d6d53587a8fb678acbfdf7522fad1d71f
edgarn7 <edgar17.neto@gmail.com>
2024-04-10 15:10:22 -0300

    adding Ionia.

---
+++ lib/ionia/ionia_characters_repository.dart
@@ -0,0 +1,27 @@
+import '../../characters.dart';
+
+class IoniaCharactersRepository {
+  static List<Characters> ioniaCharactersTable = [
+    Characters(1, "Ahri", "ahri.txt"),
+    Characters(2, "Akali", "akali.txt"),
+    Characters(3, "Irelia", "irelia.txt"),
+    Characters(4, "Ivern", "ivern.txt"),
+    Characters(5, "Jhin", "jhin.txt"),
+    Characters(6, "Karma", "karma.txt"),
+    Characters(7, "Kayn", "kayn.txt"),
+    Characters(8, "Kennen", "kennen.txt"),
+    Characters(9, "Lee Sin", "lee_sin.txt"),
+    Characters(10, "Lillia", "lillia.txt"),
+    Characters(11, "Master Yi", "master_yi.txt"),
+    Characters(12, "Wukong", "wukong.txt"),
+    Characters(13, "Rakan", "rakan.txt"),
+    Characters(14, "Sett", "sett.txt"),
+    Characters(15, "Shen", "shen.txt"),
+    Characters(16, "Syndra", "syndra.txt"),
+    Characters(17, "Varus", "varus.txt"),
+    Characters(18, "Xayah", "xayah.txt"),
+    Characters(19, "Yasuo", "yasuo.txt"),
+    Characters(20, "Yone", "yone.txt"),
+    Characters(21, "Zed", "zed.txt"),
+  ];
+}
---
+++ lib/ionia/ionia_places_repository.dart
@@ -0,0 +1,15 @@
+import '../../places.dart';
+
+class IoniaPlacesRepository {
+  static List<Places> ioniaPlacesTable = [
+    Places(1, "Ilha de Ionia", "ilha_de_ionia.txt"),
+    Places(2, "Navori", "navori.txt"),
+    Places(3, "Shon-Xan", "shon_xan.txt"),
+    Places(4, "Fae'lor", "fae_lor.txt"),
+    Places(5, "Bahrl", "bahrl.txt"),
+    Places(6, "Montanhas do Bardo", "montanhas_do_bardo.txt"),
+    Places(7, "Houhjo", "houhjo.txt"),
+    Places(8, "Oulin, Qaelin e Ralin", "oulin_qaelin_ralin.txt"),
+    Places(9, "Zhyun", "zhyun.txt"),
+  ];
+}
