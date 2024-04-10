6b91c9a0c60af1ffb0e72f3a948dc0f78dd4a0cf
edgarn7 <edgar17.neto@gmail.com>
2024-04-10 13:53:29 -0300

    adding Freljord.

---
+++ lib/freljord/freljord_characters_repository.dart
@@ -0,0 +1,20 @@
+import '../../characters.dart';
+
+class FreljordCharactersRepository {
+  static List<Characters> freljordCharactersTable = [
+    Characters(1, "Anivia", "anivia.txt"),
+    Characters(2, "Ashe", "ashe.txt"),
+    Characters(3, "Braum", "braum.txt"),
+    Characters(4, "Gnar", "gnar.txt"),
+    Characters(5, "Gragas", "gragas.txt"),
+    Characters(6, "Lissandra", "lissandra.txt"),
+    Characters(7, "Nunu e Willump", "nunu_e_willump.txt"),
+    Characters(8, "Olaf", "olaf.txt"),
+    Characters(9, "Ornn", "ornn.txt"),
+    Characters(10, "Sejuani", "sejuani.txt"),
+    Characters(11, "Trundle", "trundle.txt"),
+    Characters(12, "Tryndamere", "tryndamere.txt"),
+    Characters(13, "Udyr", "udyr.txt"),
+    Characters(14, "Volibear", "volibear.txt"),
+  ];
+}
---
+++ lib/freljord/freljord_places_repository.dart
@@ -0,0 +1,14 @@
+import '../../places.dart';
+
+class FreljordPlacesRepository {
+  static List<Places> freljordPlacesTable = [
+    Places(1, "Rakelstake", "rakelstake.txt"),
+    Places(2, "Vórtex Gélido", "vortex_gelido.txt"),
+    Places(3, "Vila dos Ursines", "vila_dos_ursines.txt"),
+    Places(4, "Montanhas Ironspike", "montanhas_ironspike.txt"),
+    Places(5, "Geleira Avarosiana", "geleira_avarosiana"),
+    Places(6, "Howling Abyss", "howling_abyss.txt"),
+    Places(7, "Lokfar", "lokfar.txt"),
+    Places(8, "Cidadela dos Praeglacius", "cidadela_dos_praeglacius.txt"),
+  ];
+}
