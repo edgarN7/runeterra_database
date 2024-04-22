8bbd5cc405d222ff1b32cbf46abe555e822ea375
edgarn7 <edgar17.neto@gmail.com>
2024-04-22 17:41:22 -0300

    adding Shurima.

---
+++ lib/shurima/shurima_characters_repository.dart
@@ -0,0 +1,17 @@
+import '../../characters.dart';
+
+class ShurimaCharactersRepository {
+  static List<Characters> shurimaCharactersTable = [
+    Characters(1, "Akshan", "akshan.txt"),
+    Characters(2, "Amumu", "amumu.txt"),
+    Characters(3, "Azir", "azir.txt"),
+    Characters(4, "K'Sante", "ksante.txt"),
+    Characters(5, "Naafiri", "naafiri.txt"),
+    Characters(6, "Nasus", "nasus.txt"),
+    Characters(7, "Rammus", "rammus.txt"),
+    Characters(8, "Renekton", "renekton.txt"),
+    Characters(9, "Sivir", "sivir.txt"),
+    Characters(10, "Taliyah", "taliyah.txt"),
+    Characters(11, "Xerath", "xarath.txt"),
+  ];
+}
---
+++ lib/shurima/shurima_places_repository.dart
@@ -0,0 +1,9 @@
+import '../../places.dart';
+
+class ShurimaPlacesRepository {
+  static List<Places> shurimaPlacesTable = [
+    Places(1, "O Disco Solar", "disco_solar.txt"),
+    Places(2, "Grande Sai", "grande_sai.txt"),
+    Places(3, "Icathia", "itachia.txt"),
+  ];
+}
