9b2a9cffb998072875dfba341308307ec37834bb
edgarn7 <edgar17.neto@gmail.com>
2024-04-22 17:51:27 -0300

    adding Targon.

---
+++ lib/targon/targon_characters_repository.dart
@@ -0,0 +1,14 @@
+import '../../characters.dart';
+
+class TargonCharactersRepository {
+  static List<Characters> targonCharactersTable = [
+    Characters(1, "Aphelios", "aphelios.txt"),
+    Characters(2, "Aurelion Sol", "aurelion_sol.txt"),
+    Characters(3, "Diana", "diana.txt"),
+    Characters(4, "Leona", "leona.txt"),
+    Characters(5, "Pantheon", "pantheon.txt"),
+    Characters(6, "Soraka", "soraka.txt"),
+    Characters(7, "Taric", "taric.txt"),
+    Characters(8, "Zoe", "zoe.txt"),
+  ];
+}
---
+++ lib/targon/targon_places_repository.dart
@@ -0,0 +1,8 @@
+import '../../places.dart';
+
+class TargonPlacesRepository {
+  static List<Places> targonPlacesTable = [
+    Places(1, "Monte Targon", "monte_targon.txt"),
+    Places(2, "Targon Primordial", "targon_primordial.txt"),
+  ];
+}
