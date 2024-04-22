692e5bb25eb622b67f22b348335acca307813ef2
edgarn7 <edgar17.neto@gmail.com>
2024-04-22 17:16:26 -0300

    adding Piltover.

---
+++ lib/piltover/piltover_characters_repository.dart
@@ -0,0 +1,14 @@
+import '../../characters.dart';
+
+class PiltoverCharactersRepository {
+  static List<Characters> piltoverCharactersTable = [
+    Characters(1, "Caitlyn", "caitlyn.txt"),
+    Characters(2, "Camille", "camille.txt"),
+    Characters(3, "Ezreal", "ezreal.txt"),
+    Characters(4, "Heimerdinger", "heimerdinger.txt"),
+    Characters(5, "Jayce", "jayce.txt"),
+    Characters(6, "Orianna", "orianna.txt"),
+    Characters(7, "Seraphine", "seraphine.txt"),
+    Characters(8, "Vi", "vi.txt"),
+  ];
+}
---
+++ lib/piltover/piltover_places_repository.dart
@@ -0,0 +1,10 @@
+import '../../places.dart';
+
+class PiltoverPlacesRepository {
+  static List<Places> piltoverPlacesTable = [
+    Places(1, "Cidades Conectadas", "cidades_conectadas.txt"),
+    Places(2, "Piltover Norte", "piltover_norte.txt"),
+    Places(3, "Piltover Sul", "piltover_sul.txt"),
+    Places(4, "Transportadores Hexdráulicos", "transportadores_hexdraulicos.txt"),
+  ];
+}
