9bc9b7834d87bf4eb70bbfa2185e84aed7e2cbb2
edgarn7 <edgar17.neto@gmail.com>
2024-05-16 20:02:40 -0300

    adding Zaun

---
+++ lib/zaun/zaun_characters_repository.dart
@@ -0,0 +1,20 @@
+import '../../characters.dart';
+
+class ZaunCharactersRepository {
+  static List<Characters> zaunCharactersTable = [
+    Characters(1, "Blitzcrank", "blitzcrank.txt"),
+    Characters(2, "Dr.Mundo", "drmundo.txt"),
+    Characters(3, "Ekko", "ekko.txt"),
+    Characters(4, "Janna", "janna.txt"),
+    Characters(5, "Jinx", "jinx.txt"),
+    Characters(6, "Renata Glasc", "renata_glasc.txt"),
+    Characters(7, "Singed", "singed.txt"),
+    Characters(8, "Twitch", "twitch.txt"),
+    Characters(9, "Urgot", "urgot.txt"),
+    Characters(10, "Viktor", "viktor.txt"),
+    Characters(11, "Warwick", "warwick.txt"),
+    Characters(12, "Zac", "zac.txt"),
+    Characters(13, "Ziggs", "ziggs.txt"),
+    Characters(14, "Zeri", "zeri.txt"),
+  ];
+}
---
+++ lib/zaun/zaun_places_repository.dart
@@ -0,0 +1,11 @@
+import '../../places.dart';
+
+class ZaunPlacesRepository {
+  List<Places> zaunPlacesTable = [
+    Places(1, "Nível Esplanada", "nivel_esplanada.txt"),
+    Places(2, "Nível Mezanino", "nivel_mezanino.txt"),
+    Places(3, "Nível Depósito", "nivel_deposito.txt"),
+    Places(4, "Transportadores Hexdráulicos", "transportadores_hexdraulicos.txt"),
+    Places(5, "Oshra Va'Zaun", "oshra_vazaun.txt"),
+  ];
+}
